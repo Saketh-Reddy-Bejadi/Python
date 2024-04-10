ddb4d3558378de00d1382453d9cfae33ee5eb00f
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-04-11 00:00:00 +0530

    modified

---
+++ Practice/Ballon d Or.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    c2 = 0
+    
+    arr = list(map(int, input().split()))
+    
+    for e in arr:
+        if e == 2:
+            c2+= 1
+            
+    if c2 % 8 == 0:
+        print("Yes")
+    else:
+        print("No")
