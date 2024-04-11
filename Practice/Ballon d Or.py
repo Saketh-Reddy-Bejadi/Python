365c26625038ee368256cb14af56dc9d177fcbb0
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-04-11 23:07:48 +0530

    modified

--- Practice/Ballon d Or.py
+++ Practice/Ballon d Or.py
@@ -1,15 +1,11 @@
 t = int(input())
-
 for _ in range(t):
     n = int(input())
     c2 = 0
-    
     arr = list(map(int, input().split()))
-    
     for e in arr:
         if e == 2:
-            c2+= 1
-            
+            c2+= 1      
     if c2 % 8 == 0:
         print("Yes")
     else:
