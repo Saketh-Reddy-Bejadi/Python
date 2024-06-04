5b9bdca6db39ba28a7b78b854af163df23ebdd04
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-06-04 21:36:39 +0530

    modified

---
+++ Practice/215.py
@@ -0,0 +1,4 @@
+nums = [3,2,3,1,2,4,5,5,6]
+k = 4
+nums.sort()
+print(nums[-k])
--- Practice/2486.py
+++ Practice/2486.py
@@ -1,7 +1,8 @@
-s = "coaching"
-t = "coding"
-r=0
-for c in s:
-    if c not in t:
-        r+=1
-print(r):
+s = "zbc"
+t = "abc"
+i=j=0
+while(i<len(s) and j<len(t)):
+    if s[i]==t[j]:
+        t+=1
+    s+=1
+print(len(t)-j)
