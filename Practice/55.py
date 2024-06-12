ca4e22c0f62670426624f0aeae47e82a8fea8d59
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-06-12 22:16:18 +0530

    modified

--- Practice/55.py
+++ Practice/55.py
@@ -2,13 +2,11 @@ def Jump(nums):
     jump=1
     for i in nums[nums[0]:]:
         if jump==0:
-            break
-        if jump>1:::
+            break::
+        if jump>1:
             jump-=1
             continue
         else:
             jump=i
     return jump==nums[-1]
-
-nums = [3,2,1,0,4]
-print(Jump(nums))
+print(Jump([3,2,1,0,4]))
