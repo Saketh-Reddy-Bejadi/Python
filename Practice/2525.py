fe6f4badde8a053870bea8e406e3874feb582e0f
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-06-12 20:20:57 +0530

    modified

--- Practice/2525.py
+++ Practice/2525.py
@@ -1,13 +1,21 @@
-length = 1000
-width = 35
-height = 700
-mass = 300
-if (length>=10**4 or width>=10**4 or height>=10**4 or mass>=10**4) and ((length*width*height)>=10**9):
-    if mass>=100:
-        print("Both")
-    :
-if mass>=100:
-    print("Heavy")
+length = 1
+width = 1
+height = 10000
+mass = 1
+b = False
 
+p = length * width * height
+
+if length >= 10000 or width >= 10000 or height >= 10000 or p >= 1000000000:
+    b = True
+
+h = mass >= 100
+
+if b and h:
+    print("Both")
+elif not b and not h:
+    print("Neither")
+elif b:
+    print("Bulky")
 else:
-    print('Neither')
+    print("Heavy")
