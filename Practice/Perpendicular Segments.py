698836c62751bc11a16df16d4d8e3ff321c96036
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-10-28 22:06:09 +0530

    modified

---
+++ Practice/Perpendicular Segments.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    xv,yv,kv=map(int,input().split())
+    te=min(xv,yv)
+    
+    print('0',te,te,'0')
+    print('0 0',te,te)
--- Practice/tempCodeRunnerFile.py
+++ Practice/tempCodeRunnerFile.py
@@ -1,17 +1,7 @@
-
 for _ in range(int(input())):
-    n,k=map(int,input().split())
-    arr=list(map(int,input().split()))
-    for i in range(k):
-        minV=arr.pop(0)
-        maxV=arr.pop(-1)
-        newValue=minV+maxV
-        l, r = 0, len(arr)
-        while l < r:
-            m = (l + r) // 2
-            if arr[m] < newValue:
-                l = m + 1
-            else:
-                r = m
-        arr.insert(l, newValue)
-    print(arr)
+    x,y,k=map(int,input().split())
+    t=min(x,y)
+    
+    print('0',t,t,'0')
+    print('0 0',t,t)
+    
