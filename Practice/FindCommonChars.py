1638814a1514ff9087b6ba28af4f40612ce4f859
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2024-10-30 15:22:44 +0530

    modified

--- Practice/FindCommonChars.py
+++ Practice/FindCommonChars.py
@@ -1,7 +1,29 @@
+def ifThen(s,w):
+    a={}
+    for c in w:
+        if c in s and c not in a:
+            a[c]=1
+        elif c in s and c in a:
+            a[c]+=1
+    return a
 def valid(words):
-    for i in range(len(words)):
-        for j in range(len(words[i])):
-            :
+    v={}
+    for c in words[0]:
+        if c in v:v[c]+=1
+        else:v[c]=1
+    for i in range(1,len(words)):
+        a=ifThen(words[0],words[i])
+        for c in v.keys():
+            if c not in a.keys():
+                v[c]=0
+            elif v[c]>a[c]:
+                v[c]=a[c]   
+        v = {k: v for k, v in v.items() if v != 0}
+    r=[]
+    for k, v in v.items():
+        for i in range(v):
+            r.append(k)
+    return r
 
-words = ["bella","label","roller"]
+words = ["cool","lock","cook"]
 print(valid(words))
